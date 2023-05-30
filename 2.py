d4b991946ae14654a87532fe908efd4d2abb2a0a
DiasKapassov <diasgoose@gmail.com>
2023-05-30 12:33:22 +0600

    2 задание

--- 2.py
+++ 2.py
@@ -1,5 +1,10 @@
-N = int(input("Enter big number: "))
-k = 0
-while 2**k <= N:
-    print(2**k)
-    k += 1
+quantity_el = int(input("Enter quantity elements in array: "))
+my_array = list(map(int, input("Enter numbers for array with SPACE: ").split(" "))) #read our array
+find_num = int(input("Enter number for find: "))
+
+index = 0
+for i in range(1, quantity_el):
+    if abs(my_array[i]-find_num) < abs(my_array[index]-find_num): #compare in absolute numbers differences
+        index=i
+
+print(f"The number {my_array[index]} is closest to the number {find_num}.")
