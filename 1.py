3612ced0affd4872f3849c597f9a961fbfd9856f
DiasKapassov <diasgoose@gmail.com>
2023-05-30 12:33:15 +0600

    1 задание

--- 1.py
+++ 1.py
@@ -1,13 +1,10 @@
-n = int(input())
-count_zero = 0
-count_one = 0
-for i in range(n):
-x = int(input())
-if x == 0:
-count_zero += 1
-else:
-count_one += 1
-if count_one > count_zero:
-print(count_zero)
-else:
-print(count_one)
+quantity_el = int(input("Enter quantity elements in array: "))
+my_array = list(map(int, input("Enter numbers for array with SPACE: ").split(" "))) #read our array
+find_num = int(input("Enter number for find: "))
+
+count = 0
+for i in range(quantity_el):
+    if my_array[i] == find_num:
+        count +=1
+
+print(f"Number {find_num} entered {count} times.")
