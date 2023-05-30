de56221e39d68449acf9f8e416af79e14b2169a2
DiasKapassov <diasgoose@gmail.com>
2023-05-30 12:36:21 +0600

    3 задание

--- 3.py
+++ 3.py
@@ -1,11 +1,31 @@
-s = int(input("Enter SUM of numbers: "))
-p = int(input("Enter Multiply of numbers: "))
+value_eng_letter = {'A': 1, 'B': 3, 'C': 3,
+                  'D': 2, 'E': 1, 'F': 4,
+                  'G': 2, 'H': 4, 'I': 1,
+                  'J': 8, 'K': 5, 'L': 1,
+                  'M': 3, 'N': 1, 'O': 1,
+                  'P': 3, 'Q': 10, 'R': 1,
+                  'S': 1, 'T': 1, 'U': 1,
+                  'V': 4, 'W': 4, 'X': 8,
+                  'Y': 4, 'Z': 10}
+value_rus_letter = {'А': 1, 'Б': 3, 'В': 1,
+                  'Г': 3, 'Д': 2, 'Е': 1,
+                  'Ё': 3, 'Ж': 5, 'З': 5,
+                  'И': 1, 'Й': 4, 'К': 2,
+                  'Л': 2, 'М': 2, 'Н': 1,
+                  'О': 1, 'П': 2, 'Р': 1,
+                  'С': 1, 'Т': 1, 'У': 2,
+                  'Ф': 10, 'Х': 5, 'Ц': 5,
+                  'Ч': 5, 'Ш': 8, 'Щ': 10,
+                  'Ъ': 10, 'Ы': 4, 'Ь': 3,
+                  'Э': 8, 'Ю': 8, 'Я': 3}
 
-for x in range(1, s+1):
-    if p % x == 0:
-        y = p // x
-        if x + y == s:
-            print("Answer: ", x, y)
-            break
-else:
-    print("It's not working!")
+given_word = input("Enter your word: ").upper() #capsular protection
+
+score = 0
+for letter in given_word:
+    if letter in value_eng_letter:
+        score += value_eng_letter[letter]
+    elif letter in value_rus_letter:
+        score += value_rus_letter[letter]
+
+print(f"Your word {given_word} gives to you {score} points!")
